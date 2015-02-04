14b14d6939f777b6d189e5d100212e1f4bd7b5da
Ben Junya <bjunya@gmail.com>
2015-02-03 20:52:35 -0800

    Added URLs to views

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,19 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('', 
+	# /polls/
 	url(r'^$', views.index, name='index'),
+
+	#/polls/otherSite
 	url(r'^otherSite', views.otherSite, name='otherSite'),
+
+	#/polls/5/
+	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+
+	#/polls/5/results
+	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+
+	#/polls/5/vite
+	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
 
